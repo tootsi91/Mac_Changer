650c46bc71ae949fcc4230c69b0321d8e0c42e08
root <tootsiats@gmail.com>
2018-09-28 00:57:48 +0300

    if statements

--- mac_changer.py
+++ mac_changer.py
@@ -3,11 +3,19 @@
 import subprocess
 import optparse
 
+
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info")
+    return options
+        # No need to return arguments because we don't use them yet
+
 
 def change_mac(interface, new_mac) :
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -15,7 +23,8 @@ def change_mac(interface, new_mac) :
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-(options, arguments) = get_arguments()
+
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
 
 
